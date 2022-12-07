83205d69bfd70fc12bbfa60ef83a6c8904f8533f
My Name <teanomhae90@gmail.com>
2022-12-07 21:04:17 +0800

    Updates Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDpBMEZmo7r28mR7eh4CUrYasEtLNpnyUQ",
-    authDomain: "testfirebaseproject1-817c0.firebaseapp.com",
-    projectId: "testfirebaseproject1-817c0",
-    storageBucket: "testfirebaseproject1-817c0.appspot.com",
-    messagingSenderId: "713364265532",
-    appId: "1:713364265532:web:2b37e350e64366bb4d8139",
-    measurementId: "G-BQNP47W2MX"
+    apiKey: "AIzaSyBWQCnPZWFyks7VvhlfWtg6Ql-kRt2x5RU",
+  authDomain: "caku-8851a.firebaseapp.com",
+  projectId: "caku-8851a",
+  storageBucket: "caku-8851a.appspot.com",
+  messagingSenderId: "685718892733",
+  appId: "1:685718892733:web:76760aaa64f0a8547107d4",
+  measurementId: "G-97JK6C9QED"
   }
  
 };
